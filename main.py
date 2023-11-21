28005249bdefcba697ab2221764cf12c06d47538
SergeiMischenko <main0serg@gmail.com>
2023-11-21 20:12:17 +0600

    added random spawn enemy and fight and protected methods

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from player import create_player
-from enemy import Enemy, create_enemy
-from my_coloram import GREEN, RED, YELLOW, MAGENTA
+from player import Player
+from enemy import Enemy
+from my_coloram import MAGENTA, GREEN, YELLOW, RED
 
 
 def main():
-    print(f"{GREEN}Приветствуем вас в игре RPG Danger, тут вам предстоит создать своего персонажа.")
+    print(f"{GREEN}Приветствуем вас в игре RPG Danger, сейчас вам предстоит создать своего персонажа.")
     input(f"{GREEN}Нажмите " + f"{RED}ENTER" + f"{GREEN} чтобы продолжить...")
     print('-' * 35)
-    player = create_player()
-    enemy_list = create_enemy()
+    player = Player.create_player()
+    enemy_list = Enemy.create_enemy()
     while True:
-        enemy_list = Enemy.get_list_live_enemy(enemy_list)
         if not player.hp or not enemy_list:
             print(f"\n\t{RED}!!!-----ИГРА ОКОНЧЕНА-----!!!")
             break
         Enemy.print_list_enemy(enemy_list)
-        target = valid_target(input(f"{YELLOW}Выберите кого атаковать: ").capitalize(), enemy_list)
-        player.attack(enemy_list[target])
-        enemy_list[0].attack(player)
+        fight(enemy_list, player)
 
 
 def valid_target(amount, enemy_list):
@@ -34,6 +31,12 @@ def valid_target(amount, enemy_list):
     if amount.isalpha():
         amount = enemy_race_list.index(amount) + 1
     return int(amount) - 1
+
+
+def fight(enemy_list, player):
+    target = valid_target(input(f"{YELLOW}Выберите кого атаковать: ").capitalize(), enemy_list)
+    player.attack(enemy_list[target])
+    Enemy.enemy_attacks(enemy_list, player)
 
 
 if __name__ == "__main__":
